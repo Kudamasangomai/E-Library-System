d9a8329fbfaaa07731c4f3da410d4420081a7a18
Kuda Masangomai <kudam775@gmail.com>
2022-04-13 13:44:26 +0200

    initial commit

---
+++ uzapi/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [  
+
+		path('',views.getroutes),
+		path('books/',views.getbooks),
+		path('author/',views.getauthor),
+		path('book/<str:pk>',views.getbook),
+		path('category/',views.getcategory)
+
+
+]
